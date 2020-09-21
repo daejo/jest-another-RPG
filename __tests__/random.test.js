7c29bb68288eeeace261d81370d83d699f6edc2a
daejo <daejo@icloud.com>
2020-09-21 02:01:35 -0700

    tests potion.js and random.js

---
+++ __tests__/random.test.js
@@ -0,0 +1,7 @@
+//* TEST SET UP*//
+const randomNumber = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
