d3d8a7c3fe65b92dd2de66cb6b6161e6c3287e6a
daejo <daejo@icloud.com>
2020-09-21 15:32:50 -0700

    passes isAlive test function

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,8 @@
 const Potion = require('../lib/Potion');
-
-jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
-
 const Player = require('../lib/Player');
+jest.mock('../lib/Potion.js');
+// console.log(new Potion());
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -35,4 +33,20 @@ test('creates a player object', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
   });
